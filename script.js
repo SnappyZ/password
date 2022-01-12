3a2039431993f769a0b42cbe694d05406f60fcb8
Azanini <austinzanini@gmail.com>
2022-01-11 22:19:02 -0800

    correcting the starter code that I tweaked

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 // Assignment code here
-
-// Password length Prompt //
 function generatePassword() {
   var confirmLength = window.prompt("Please choose the desired length of your password. It must be between 8 and 128 characters.");
   if (confirmLength === true) {
-    confirmLength = alert ("Add value.");
-  } else if (confirmLength > 8 || confirmLength < 128) {
-  alert ("This is not a valid value, it must be between 8 and 128 characters.");    
+    confirmLength = alert("Add a value.");
+  } else if (confirmLength > 8 || confirmLength < 128) { 
+  alert ("This is not a valid answer, please choose a value between 8 and 128.");
   }
   return generatePassword();
 }
