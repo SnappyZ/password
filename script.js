c220fcb07056295485e32af7e444161f7737c38b
Azanini <austinzanini@gmail.com>
2022-01-11 21:52:04 -0800

    password length prompt

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment code here
 
+// Password length Prompt //
+function generatePassword() {
+  var confirmLength = window.prompt("Please choose the desired length of your password. It must be between 8 and 128 characters.");
+  if (confirmLength === true) {
+    confirmLength = alert ("Add value.");
+  } else if (confirmLength > 8 || confirmLength < 128) {
+  alert ("This is not a valid value, it must be between 8 and 128 characters.");    
+  }
+  return generatePassword();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
